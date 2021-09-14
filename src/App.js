705e8ea05d1dec91aaa44adf7e419805ee4a03a3
Taras-Kolomiiets <taraskolomiiets8991@gmail.com>
2021-09-14 13:45:24 +0300

    lesson 49

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Profile from "./components/Profile/Profile";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import UsersContainer from "./components/Users/Users";
+import UsersContainer from "./components/Users/UsersContainer";
 import { Route } from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import NavbarContainer from "./components/Navbar/NavbarContainer";
