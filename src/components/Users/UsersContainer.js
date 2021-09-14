e8e498dbca2b4855dbab9e66f1b3e19912747447
Taras-Kolomiiets <taraskolomiiets8991@gmail.com>
2021-09-14 21:00:34 +0300

    lesson 55

--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,10 +1,19 @@
 import Users from "./Users";
 import { connect } from "react-redux";
-import { followAC, setUsersAC, unfollowAC } from "../../redux/users-reducer";
+import {
+  followAC,
+  setCurrentPageAC,
+  setUsersAC,
+  unfollowAC,
+  setTotalPageCountAC,
+} from "../../redux/users-reducer";
 
 const mapStateToProps = (state) => {
   return {
     users: state.usersPage.users,
+    pageSize: state.usersPage.pageSize,
+    totalCount: state.usersPage.totalUsersCount,
+    currentPage: state.usersPage.currentPage,
   };
 };
 
@@ -18,6 +27,12 @@ const mapDispatchToProps = (dispatch) => {
     },
     setUsers: (users) => {
       dispatch(setUsersAC(users));
+    },
+    setCurrentPage: (currentPage) => {
+      dispatch(setCurrentPageAC(currentPage));
+    },
+    setTotalUsersCount: (totalCount) => {
+      dispatch(setTotalPageCountAC(totalCount));
     },
   };
 };
